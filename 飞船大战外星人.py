852294f36e502bc7bce8ef8d0a3033cb5504940d
milaxj <152498214+milaxj@users.noreply.github.com>
2026-05-05 23:48:32 +0800

    创建 飞船大战外星人.py
    12.4.1

---
+++ 飞船大战外星人.py
@@ -0,0 +1,35 @@
+import sys
+import pygame
+
+def ship(screen):
+    tuxiang = screen
+    #加载图像
+    image = pygame.image.load('你的图像目录')
+    #获取外接矩形
+    rect = image.get_rect()
+    screen_rect = screen.get_rect()
+    #放置在底部中央
+    rect.centerx = screen_rect.centerx
+    rect.bottom = screen_rect.bottom
+    #指定位置绘制
+    screen.blit(image,rect)
+
+def run_game():
+    pygame.init()
+    pinmu = pygame.display.set_mode((1200，800))
+    beijin = (230，230,230,)
+    pygame.display.set_caption("飞船大战外星人")
+    
+    #主循环
+    while True:
+        #监听鼠标
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        #重绘屏幕
+        screen.fill(beijin)
+
+        #让最近的绘制可见
+        pygame.display.flip()
+
+run_game()
